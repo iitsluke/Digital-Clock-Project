f111cb9aa06c61395c175c52827b8c937905e7f4
Lukas Vasco <34105119+iitsluke@users.noreply.github.com>
2022-02-06 20:50:38 -0500

    Added divs to html file and added day varible with connection to its function

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 const currentTime = new Date();
 console.log(currentTime)
+function theClock() {
+
+  //added the day of the week with a varible connected to a function with an array list of items
+  const day = daySelector(currentTime.getDay());
+
+  //Created Month & Year varible but now fuctions must be created to connect 
+  /*
+  const month;
+  const year; 
+  */
+
+  const dateDisplay = document.getElementById('date-display');
+  dateDisplay.textContent = `Today is` + ` ${day}`
+}
+theClock();
+
+
+function daySelector(index) {
+  const days = [ 
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  return days[index];
+}
 
